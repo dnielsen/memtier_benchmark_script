bc77fbb5b125df77933a24f42348fa4a66395f4e
Dave Nielsen <dnielsen@gmail.com>
2016-01-13 15:59:46 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
-# code goes here
+#
+
+import subprocess
+
+def execute(command):    
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    # nothing intersting in stdout, all the action is in stderr
+    lines_iterator = iter(popen.stderr.readline, b"")
+    for line in lines_iterator:
+        print(">" + line) # or match and parse it...
+
+execute(["memtier_benchmark", "--hide-histogram", "--test-time=60"]) # add relevant switches here
